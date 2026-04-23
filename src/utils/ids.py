a82baa2d24d7469bc551592fb2c1bcfd864dc5b6
Daranton <104932961+Daranton@users.noreply.github.com>
2026-04-23 16:35:17 +0100

    chore: cleaned up project structure against .gitignore

--- src/utils/ids.py
+++
@@ -1,9 +0,0 @@
-# connected to main.py 
-# "from src.utils.ids import generate_order_id"
-
-import uuid
-
-
-def generate_order_id() -> str:
-    """Return a new unique order ID."""
-    return str(uuid.uuid4())
--- src/utils/validation.py
+++
@@ -1,6 +0,0 @@
-from src.utils.models import VALID_STATUSES
-
-
-def validate_status(status: str) -> bool:
-    """Return True if status is a recognised order status."""
-    return status.lower() in VALID_STATUSES
